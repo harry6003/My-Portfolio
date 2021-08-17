7d51436b2b655edfe0da6e6ea77ed72da479cdf6
Himanshu <agrawalhimanshu814@gmail.com>
2021-08-17 23:46:58 +0530

    made basic structure

---
+++ script.js
@@ -0,0 +1,13 @@
+var scrollToTopBtn = document.getElementById("firstdown");
+var rootElement = document.documentElement;
+
+function scrollToTop() {
+    // Scroll to top logic
+    rootElement.scrollTo({
+        top: 1000,
+        behavior: "smooth",
+    });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+// typing animation
